b33e09c2165a3c527ef22d17fefb28463fe4e288
Sharif Elkassed <selkasse@gmu.edu>
2023-09-20 08:28:43 -0400

    add custom theme

---
+++ force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
@@ -0,0 +1,10 @@
+import { LightningElement } from "lwc";
+
+/**
+ * @slot header
+ * @slot footer
+ * @slot another
+ * @slot default
+ */
+
+export default class CustomThemeLayout extends LightningElement {}
