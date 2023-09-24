a75bfda663d93dd14989642c005c7c8e5926f6e6
Sharif Elkassed <selkasse@gmu.edu>
2023-09-24 17:14:19 -0400

    refine nav styling WIP

--- force-app/main/default/lwc/nav/nav.js
+++ force-app/main/default/lwc/nav/nav.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import FORM_FACTOR from "@salesforce/client/formFactor";
 
-export default class Nav extends LightningElement {}
+export default class Nav extends LightningElement {
+  connectedCallback() {
+    console.log(`FORM_FACTOR: ${FORM_FACTOR}`);
+  }
+}
