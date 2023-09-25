016a1da3749d332cd0be33a2933c8145cbff3186
Sharif Elkassed <selkasse@gmu.edu>
2023-09-25 00:35:21 -0400

    base navbar complete. multi-level nav WIP

--- force-app/main/default/lwc/nav/nav.js
+++ force-app/main/default/lwc/nav/nav.js
@@ -1,4 +1,9 @@
-import { LightningElement, api } from "lwc";
-import FORM_FACTOR from "@salesforce/client/formFactor";
+import { LightningElement } from "lwc";
 
-export default class Nav extends LightningElement {}
+export default class Nav extends LightningElement {
+  handleHamburgerClick() {
+    const nav = this.template.querySelector(`.nav`);
+
+    nav.classList.toggle(`responsive`);
+  }
+}
